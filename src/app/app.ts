619f6d65d3482da00e16061be667ce7dc256ab41
Gokudoma <stephan_wacker@hotmail.com>
2025-07-05 13:07:57 +0200

    basic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { HeroSection } from './components/hero-section/hero-section';
+import { AboutMe } from './components/about-me/about-me';
+import { MySkills } from './components/my-skills/my-skills';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Header,
+    HeroSection,
+    AboutMe,
+    MySkills,
+    Portfolio,
+    Contact,
+    Footer,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'portfolio';
+  title = 'portfolio';
 }
