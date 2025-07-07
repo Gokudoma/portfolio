754a67e73d9be2f08fd089e994f8fad7c85a6088
Gokudoma <stephan_wacker@hotmail.com>
2025-07-07 12:11:55 +0200

    skillimg

--- src/app/pages/imprint/imprint.ts
+++ src/app/pages/imprint/imprint.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  standalone: true,
   templateUrl: './imprint.html',
   styleUrl: './imprint.scss'
 })
 export class Imprint {
-
+  email = 'stephanwacker318@gmail.com';
 }
