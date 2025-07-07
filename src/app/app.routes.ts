1aa3f82320b3b99443c229cb5857d4863b20dc8d
Gokudoma <stephan_wacker@hotmail.com>
2025-07-07 08:55:26 +0200

    imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Imprint } from './pages/imprint/imprint';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'impressum', component: Imprint }
+];
