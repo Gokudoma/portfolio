59754d26e66ed3114563f98605166179d31a3379
Gokudoma <stephan_wacker@hotmail.com>
2025-07-08 09:25:30 +0200

    header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
