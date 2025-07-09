939157152ca9b42028135132101587f4d1289a48
Gokudoma <stephan_wacker@hotmail.com>
2025-07-09 11:04:18 +0200

    hero-component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
