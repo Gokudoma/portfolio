21177255c88360cd2f03e88b1b62aa3a5a2d1f9d
Gokudoma <stephan_wacker@hotmail.com>
2025-07-21 10:08:30 +0200

    contact form

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
