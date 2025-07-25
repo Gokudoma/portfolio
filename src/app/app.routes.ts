4c748bbce101c83ff46e73e78d8cda09c39973ed
Gokudoma <stephan_wacker@hotmail.com>
2025-07-25 11:14:50 +0200

    contact-change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './pages/main-content/main-content';
 import { Imprint } from './pages/imprint/imprint';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent },
-  { path: 'impressum', component: Imprint }
+  { path: 'impressum', component: Imprint },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
