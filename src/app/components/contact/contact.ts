dcf9f9d7d93539abfdda558f90f04bff021216b7
Gokudoma <stephan_wacker@hotmail.com>
2025-09-17 10:27:01 +0200

    bug fixes

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslateModule, FormsModule, CommonModule],
+  imports: [TranslateModule, FormsModule, CommonModule, RouterLink],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
